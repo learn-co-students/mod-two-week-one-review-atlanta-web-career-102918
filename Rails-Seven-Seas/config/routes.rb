42f00f47e01c6149b7a104a76a3271eee46b57a4
wgeorge1990 <williameverettgeorge@icloud.com>
2018-11-26 13:55:14 -0500

    Done.

--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :seas
+
+  get 'welcome/index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 
 end
